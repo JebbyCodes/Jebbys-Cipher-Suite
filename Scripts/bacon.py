acad50b541a76f481aedd09616dc1483d37b5708
Neededwolf <126186331+Neededwolf@users.noreply.github.com>
2025-10-28 10:12:03 +0000

    Add files via upload

---
+++ Scripts/bacon.py
@@ -0,0 +1,22 @@
+def main():
+    text  = str(input(">> ")).lower().split(" ")
+    cipher(text)
+
+def cipher(text):
+    plaintext=""
+    for let in text:
+        total = 0
+        for value in range(len(let)):
+            char=let[value]
+
+            if char == "b":
+                total += 2**(5-(value+1))
+
+
+        plaintext += chr(total+65)
+
+    print(plaintext)
+
+if __name__ == "__main__":
+
+    main()
