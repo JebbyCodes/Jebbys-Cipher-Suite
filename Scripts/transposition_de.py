1423025addb1b4cfe3959f98ec5f0907a707601e
JebbyCodes <125154831+JebbyCodes@users.noreply.github.com>
2025-11-02 12:35:33 +0000

    update transposition_de.py

--- Scripts/transposition_de.py
+++ Scripts/transposition_de.py
@@ -28,15 +28,18 @@ def main():
     textbox_frame.grid_columnconfigure(0, weight=1, uniform="textcols")
     textbox_frame.grid_columnconfigure(1, weight=1, uniform="textcols")
 
-    label_cipher = ctk.CTkLabel(textbox_frame, text="Ciphertext:", font=ctk.CTkFont(size=16, weight="bold"))
-    label_cipher.grid(row=0, column=0, padx=20, pady=(10,0))
-
-    label_plain = ctk.CTkLabel(textbox_frame, text="Plaintext:", font=ctk.CTkFont(size=16, weight="bold"))
-    label_plain.grid(row=0, column=1, padx=20, pady=(10,0))
+    # -- cipher processing -- #
 
     textbox_cipher = ctk.CTkTextbox(textbox_frame, font=("Courier New", 12))
     textbox_cipher.grid(row=1, column=0, padx=20, pady=20, sticky="nsew")
     textbox_cipher.configure(height=300, width=300)
+
+    label_cipher = ctk.CTkLabel(textbox_frame, text="Ciphertext:", font=ctk.CTkFont(size=16, weight="bold"))
+
+    btn_cipher_process = ctk.CTkButton(textbox_frame, text="Process Transposition Cipher", command=lambda: None)
+    btn_cipher_process.grid(row=2, column=0, pady=10)
+
+    # -- END cipher processing END -- #
 
     def update_plain(event=None):
         text = textbox_cipher.get("1.0", ctk.END).strip()
@@ -47,10 +50,23 @@ def main():
 
     textbox_cipher.bind("<KeyRelease>", update_plain)
 
+    # -- plain processing -- #
+
     textbox_plain = ctk.CTkTextbox(textbox_frame, font=("Courier New", 12))
     textbox_plain.grid(row=1, column=1, padx=20, pady=20, sticky="nsew")
     textbox_cipher.configure(height=300, width=300)
     textbox_plain.configure(state="disabled")
+
+    btn_plain_process = ctk.CTkButton(textbox_frame, text="Process Plaintext", command=lambda: None)
+    btn_plain_process.grid(row=2, column=1, pady=10)
+
+    label_plain = ctk.CTkLabel(textbox_frame, text="Plaintext:", font=ctk.CTkFont(size=16, weight="bold"))
+
+    # -- END plain processing END -- #
+
+
+    label_cipher.grid(row=0, column=0, padx=20, pady=(10,0))
+    label_plain.grid(row=0, column=1, padx=20, pady=(10,0))
 
 
     root.mainloop()
